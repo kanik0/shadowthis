9e72ba83bbf33d65ad8915471edb4be74def83f3
Massimo Bedini <bedini.massimo@gmail.com>
2018-03-16 12:41:52 +0100

    Update shadowthis.py

--- shadowthis.py
+++ shadowthis.py
@@ -5,8 +5,8 @@ from unicode_spaces import unicode_spaces
 ONE = unicode_spaces["ZERO WIDTH SPACE"]
 ZERO = unicode_spaces["ZERO WIDTH NO-BREAK SPACE"]
 
-def shadow(msg, userid):
-    binary_id = bin(userid)[2:]
+def shadow(msg, userid):                                      # Inserts an hidden userid after
+    binary_id = bin(userid)[2:]                               # the first word in a string
     secret = u""
     for i in binary_id:
         if i == '0':
@@ -18,14 +18,14 @@ def shadow(msg, userid):
     output = u" ".join(output)
     return output
   
-def deshadow(msg):
-    secret = msg.split()[1]
+def deshadow(msg):                                            # Gets the hidden userid back
+    secret = msg.split()[1]                                   # from a previously encoded message
     secret = secret.replace(ONE, "1")
     secret = secret.replace(ZERO, "0")
     return int(secret, 2)
 
 
 if __name__ == '__main__':
-    encoded = shadow('ciao, come stai?', 9666)
-    print(encoded)
-    print(deshadow(encoded))
+    encoded = shadow('ciao, come stai?', 9666)                # Ex: hides the code '9666' into
+    print(encoded)                                            # a string and gets the code back
+    print(deshadow(encoded))                                  # from the encoded string
